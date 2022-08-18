4cb66d882bd82ff9dd3872712045ec91611b8757
Patrick-Groot <patrick@candis.io>
2022-08-18 21:59:41 +0200

    add input field

--- index.js
+++ index.js
@@ -5,16 +5,36 @@ for (let i = 1; i < 6; i++) {
 
 const rollButton = document.getElementById("roll-button");
 rollButton.addEventListener("click", rollDice);
+
 let intervalId;
+let diceValues;
 let resultArray;
-let rollCounter;
+let rollCount = 1;
+
+const rollCounter = document.getElementById("roll-counter");
+rollCounter.innerHTML = `Roll ${rollCount} / 3`
+const keepDiceInput = document.getElementById("keep-input");
+
 
 changeDiceImageRandom()
 
-function rollDice() {
+function rollDice() {    
   intervalId = setInterval(changeDiceImageRandom,30);
-  setTimeout(() => clearInterval(intervalId), 1000);
+  setTimeout(() => {
+    clearInterval(intervalId);
+    endOfRoll()}, 1000);  
 }
+
+function endOfRoll(){
+    if (rollCount < 3) rollCount ++;
+    rollCounter.innerHTML = `Roll ${rollCount} / 3`;
+    resultArray = { ...diceValues };
+    console.log(resultArray);
+    keepDiceInput.classList.toggle("hidden");
+}
+
+// After keep-dice input given again toggle hidden
+
 function createDiceImage(num) {
   const diceImg = document.createElement("img");
   diceImg.src = `/Images/Alea_${num}.png`;
@@ -27,13 +47,13 @@ function changeDiceImage(elem, num) {
 }
 
 function changeDiceImageRandom() {
-  resultArray = dice.map((elem) => {
+  diceValues = dice.map((elem) => {
     let randomNum = Math.floor(Math.random() * 6 + 1);
     if (elem.hasChildNodes()) {
       elem.innerHTML = "";
     }
     changeDiceImage(elem, randomNum);
-    return { value: randomNum };
+    return {value: randomNum};
   });
-  console.log(resultArray);
+  //console.log(diceValues);
 }
